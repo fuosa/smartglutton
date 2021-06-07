69b97cb785883f3534898990ae8e298e7c53ac2e
fuosa <tokyo18osamu@yahoo.co.jp>
2021-06-07 15:53:53 +0900

    商品出品機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,9 +5,24 @@ before_action :move_to_index, except: [:index, :show]
   end
 
   def new
+    @item = Item.new
   end
 
+  def create 
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+
   private 
+  def item_params
+    params.require(:item).permit(:item_name, :description, :price, :image).merge(user_id: current_user.id) 
+  end
+
   def move_to_index
     unless user_signed_in?
       redirect_to action: :index

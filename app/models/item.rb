88e23d0526278e346ee188d55ba311d97c179d73
fuosa <tokyo18osamu@yahoo.co.jp>
2021-06-07 18:40:47 +0900

    itemモデルにバリデーションを記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,4 +3,11 @@ class Item < ApplicationRecord
   has_many :ordered_items
   belongs_to :categoryies
   has_one_attached :image
+
+  with_options presence: true do
+    validates :item_name
+    validates :description
+    validates :price
+    validates :image
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "items#index"
-  resources :users, only: [:edit, :update]
+  resources :users, only: [:show, :edit, :update]
   resources :items, only: [:index, :create, :new]
 end

b6790d7c888c5526e900b4a99a9339bd1649f27e
fuosa <tokyo18osamu@yahoo.co.jp>
2021-06-17 19:00:49 +0900

    viewの調整

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -5,15 +5,26 @@ class OrdersController < ApplicationController
   end
 
   def new 
+    @order_info = OrderInfo.new
   end
 
   def create
+    @order_info = OrderInfo.new(@order_info_params)
+      if @order_info.valid?
+        @order_info.save
+        redirect_to root_path
+      else
+        render :index
+      end
   end
 
 
 
   private
-  
+
+  def order_info_params
+      params.require(:order_info).permit(:item_qty, :item_status, :bill, :payment_type, :receive_type, :phone).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+  end
   # def orderd_item_params
   #   params.require(:ordered_item).permit(:item_qty, :item_status, :payment_type, :received_type, :phone)
   #   .merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
--- app/models/order_info.rb
+++ app/models/order_info.rb
@@ -1,4 +1,27 @@
 class OrderInfo
-  include ActiveModel::modules
+  include ActiveModel::Model
   attr_accessor :item_qty, :item_status, :bill, :payment_type, :receive_type, :phone, :user_if, :item_id
+
+  with_options presence: true do
+    validates :item_qty
+    validates :item_status
+    validates :bill
+    validates :payment_type
+    validates :receive_type
+    validates :phone
+  end
+
+  with_options numericality: {other_than: 0, message: "can't be blank"} do
+    validates :item_qty
+    validates :item_status
+    validates :bill
+    validates :payment_type
+    validates :receive_type
+    validates :phone
+  end
+
+  def save 
+    order = Order.create(user_id: user_id, item_id: item_id)
+    OrderedItem.create(item_qty: item_qty, item_status: item_status, bill: bill, payment_type: payment_type, receive_type: receive_type, phone: phone)
+  end
 end

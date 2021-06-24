7ecda7df19b646c575bcf5ca732128aff0c13b46
fuosa <tokyo18osamu@yahoo.co.jp>
2021-06-24 17:11:51 +0900

    Formオブジェクトの実装完了

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,27 +3,34 @@ class OrdersController < ApplicationController
 
 
   def index 
-  end
-
-  def new 
+    @order_info = OrderInfo.new
   end
 
 
   def create
-    @order = Order.create(order_params)
-    OrderedItem.create(ordered_item_params)
-    redirect_to root_path
+    @order_info = OrderInfo.new(order_params)
+    if @order_info.valid?
+      @order_info.save
+      redirect_to root_path
+    else
+      render :index
+    end
+
+
+    # @order = Order.create(order_params)
+    # OrderedItem.create(ordered_item_params)
+    # redirect_to root_path
   end
 
 
   private
 
   def order_params
-    params.permit(:order).merge(user_id: current_user.id, item_id: params[:item_id])
+    params.require(:order_info).permit(:item_qty, :payment_type, :received_type, :phone).merge(user_id: current_user.id, item_id: params[:item_id])
+    # params.permit(:order).merge(user_id: current_user.id, item_id: params[:item_id])
   end
 
-  def ordered_item_params 
-    params.permit(:item_qty, :payment_type, :received_type, :phone).merge(order_id: @order.id)
-  end
+  # def ordered_item_params 
+  #   params.permit(:item_qty, :payment_type, :received_type, :phone).merge(order_id: @order.id)
 
 end
--- app/models/order_info.rb
+++ app/models/order_info.rb
@@ -1,13 +1,13 @@
 class OrderInfo
-  include AciveModel::Model
-  attr_accesor :item_qty, :payment_type, :received_type, :phone, :user_id, :item_id
+  include ActiveModel::Model
+  attr_accessor :item_qty, :payment_type, :received_type, :phone, :user_id, :item_id
 
 
-  with_optipns presence: true do 
+  with_options presence: true do 
     validates :item_qty, numericality: {other_than: 0, message: "can't be blank"}
     validates :payment_type, numericality: {other_than: 0, message: "can't be blank"}
     validates :received_type, numericality: {other_than: 0, message: "can't be blank"}
-    valisates :phone
+    validates :phone
     validates :user_id
     validates :item_id
   end
@@ -15,7 +15,7 @@ class OrderInfo
   def save 
     order = Order.create(user_id: user_id, item_id: item_id)
 
-    Orderd_item.create(item_qty: item_qty, payment_type: payment_type, received_type: received_type, phone: phone, order_id: order.id)
+    OrderedItem.create(item_qty: item_qty, payment_type: payment_type, received_type: received_type, phone: phone, order_id: order.id)
   end
 
 end

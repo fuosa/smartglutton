c0459ff0a4f0e599f9065ba9fc51d5867f57466c
fuosa <tokyo18osamu@yahoo.co.jp>
2021-06-24 16:43:56 +0900

    バリデーションの記述

--- app/models/order_info.rb
+++ app/models/order_info.rb
@@ -3,4 +3,16 @@ class OrderInfo
   attr_accesor :item_qty, :payment_type, :received_type, :phone, :user_id, :item_id
 
 
+  with_optipns presence: true do 
+    validates :item_qty, numericality: {other_than: 0, message: "can't be blank"}
+    validates :payment_type, numericality: {other_than: 0, message: "can't be blank"}
+    validates :received_type, numericality: {other_than: 0, message: "can't be blank"}
+    valisates :phone
+    validates :user_id
+    validates :item_id
+  end
+
+  
+
+
 end

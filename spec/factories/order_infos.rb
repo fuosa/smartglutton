fa986f51836eb40f5eabb312a38c93f75474db33
fuosa <tokyo18osamu@yahoo.co.jp>
2021-06-25 13:31:43 +0900

    factorybotの設定

--- spec/factories/order_infos.rb
+++ spec/factories/order_infos.rb
@@ -1,5 +1,9 @@
 FactoryBot.define do
   factory :order_info do
+    item_qty { 1 }
+    payment_type { 1 }
+    received_type { 1 }
+    phone { 09012345678 }
     
   end
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,13 @@
 FactoryBot.define do
   factory :user do
-    
+    nickname {'トム123'}
+    last_name { '田中' }
+    first_name { '一郎' }
+    last_name_kana { 'タナカ' }
+    first_name_kana { 'イチロウ' }
+    data_of_birth { '1980-01-01' }
+    email { 'test@test'}
+    password { 'test12345'}
+    password_confirmation { password }
   end
 end
